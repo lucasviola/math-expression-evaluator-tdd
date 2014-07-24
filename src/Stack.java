ba6826f1cd9915650cada65f63f45523c29374d4
fernando.rreggio <fernando.rreggio@gmail.com>
2014-07-24 15:28:54 -0300

    creating Evaluator

--- src/Stack.java
+++ src/Stack.java
@@ -20,5 +20,5 @@ public class Stack {
         return members.get(members.size() - 1);
     }
 
-    
+
 }
---
+++ test/EvaluatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.*;
+
+public class EvaluatorTest {
+
+    @Test
+    public void givenTwoNumbersAndOneOperatorConvertToPostfix() throws Exception {
+       Stack stack = new Stack();
+       stack.push(2);
+       stack.push(+);
+       stack.push(5);
+
+
+    }
+}
