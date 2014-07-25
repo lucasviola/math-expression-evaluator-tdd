a3f660f6aa1e7d808c05be349d54e691f929267f
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-07-24 22:07:07 -0300

    Implementing Evaluator

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -2,9 +2,15 @@ public class Evaluator {
 
     public void fromInfixToPostfix(Stack stack) {
         for(int i = 0; i < stack.members.size(); i++){
-            if(stack.members.get(i).equals("+"))
+            if(stack.members.get(i).equals("+")){
                 stack.members.remove(i);
                 stack.push("+");
+            }
+
+            if(stack.members.get(i).equals("-")){
+                stack.members.remove(i);
+                stack.push("-");
+            }
         }
     }
 }
--- test/EvaluatorTest.java
+++ test/EvaluatorTest.java
@@ -17,6 +17,51 @@ public class EvaluatorTest {
         Object result = stack.members.get(1);
 
         assertEquals("+", result);
+    }
 
+    @Test
+    public void givenOneOperatorAndTwoNumbersShouldMoveOperatorToTopAndRearrange() throws Exception {
+        Stack stack = new Stack();
+        Evaluator evaluator = new Evaluator();
+        stack.push(1);
+        stack.push("+");
+        stack.push(2);
+
+        evaluator.fromInfixToPostfix(stack);
+        Object result = stack.members.get(2);
+
+        assertEquals("+", result);
+    }
+
+    @Test
+    public void givenOneOperatorAndThreeNumbersShouldMoveOperatorToTopAndRearrange() throws Exception {
+        Stack stack = new Stack();
+        Evaluator evaluator = new Evaluator();
+        stack.push(1);
+        stack.push("-");
+        stack.push(2);
+        stack.push(11);
+
+        evaluator.fromInfixToPostfix(stack);
+        Object result = stack.members.get(3);
+
+        assertEquals("-", result);
+    }
+
+        @Test
+    public void givenTwoOperatorsAndTwoNumbersShouldMoveOpsToTopAndReArrange() throws Exception {
+        Stack stack = new Stack();
+        Evaluator evaluator = new Evaluator();
+        stack.push("-");
+        stack.push(1);
+        stack.push("+");
+        stack.push(11);
+
+        evaluator.fromInfixToPostfix(stack);
+        Object result = stack.members.get(2);
+        Object secondResult = stack.members.get(3);
+
+        assertEquals("+", result);
+        assertEquals("-", secondResult);
     }
 }
